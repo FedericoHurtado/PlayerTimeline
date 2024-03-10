8c2a91888e20865bb141639db81affba4df8b1a9
FedericoHurtado <121637596+FedericoHurtado@users.noreply.github.com>
2024-03-10 13:29:43 -0400

    Fixed bug in switching pages.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,24 @@ import React from 'react'
 import axios from "axios"
 import { useEffect, useState } from "react";
 import PlayerDetails from './PlayerDetails';
+import { useNavigate } from 'react-router-dom';
 
+
+// Home page: where user will enter their league ID
 function Home() {
 
     // set 3 local values -> LeagueID, leagueValid, and errorMesage
     const [league_id, setLeagueId] = useState(''); // initially create empty string as id
     const [leagueValid, setLeagueValid] = useState(false); // initially make league invalid
     const [errorMesage, setErrorMessage] = useState(''); // empty error message
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Reset leagueValid to false when the component renders
+        setLeagueValid(false);
+    }, []);
+
 
     // when text changes, update the league id and reset error
     const handleTextChange = (event) => {
@@ -16,11 +27,7 @@ function Home() {
       setErrorMessage('');
     }
   
-    /**
-     * On click: search for league and make sure it's valid. If the 
-     * league is valid, move on to next screen. If the league is not
-     * valid, display an error message. 
-     */
+    // on click: update the league ID, and if it is valid move to details page
     async function handleSearchClick() {
       try {
         // call POST to set league ID value
@@ -32,6 +39,7 @@ function Home() {
         // if valid, set leagueValid
         if (valid.data == true) {
           setLeagueValid(true);
+          navigate("/PlayerDetails");
         } else {
           // if not, set error message
           setErrorMessage("Invalid league ID.")
@@ -44,9 +52,12 @@ function Home() {
 
     // if league id is found to be valid, switch to PlayerDetails
     if (leagueValid) {
-        return <PlayerDetails league_id={league_id} />;
-    }
+      //setLeagueValid(false); // reset home page
+      return <PlayerDetails league_id={league_id} />;
+  }
 
+
+    // if league is not valid, return the searchbox
     return ( <div className="App">
     <h1> Please enter your league ID. </h1>
     <h3>Current ID: {league_id} </h3>
