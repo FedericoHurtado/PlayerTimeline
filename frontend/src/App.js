2902135da18381a62ddbfd81edcad95c3502fe66
FedericoHurtado <121637596+FedericoHurtado@users.noreply.github.com>
2024-03-10 11:33:00 -0400

    Clean up homepage in prep for creating 2 pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,39 @@ function App() {
 
   useEffect(() => {
     axios.get("http://localhost:3001/timeline/league").then((response) => {
-      setLeagueId(response.data);
+      //setLeagueId(response.data);
     })
   })
 
+  const handleInputChange = (event) => {
+    setLeagueId(event.target.value);
+  }
+
+  /**
+   * On click: search for league and make sure it's valid. If the 
+   * league is valid, move on to next screen. If the league is not
+   * valid, display an error message. 
+   */
+  const handleSearchClick = () => {
+    // set the local league id 
+    setLeagueId("1234");
+
+    // call the backend api POST /league endpoint
+    
+
+
+  }
+
   return <div className="App">
     <h1> Please enter your league ID. </h1>
+    <h3>Current ID: {league_id} </h3>
+    <input
+        type="text"
+        value={league_id}
+        onChange={handleInputChange}
+        placeholder="Enter League ID"
+      />
+      <button onClick={handleSearchClick}>Search</button>
   </div>;
 }
 
