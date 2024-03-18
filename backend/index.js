cefcb42eb33ff251ffce4eceaf446da826a57843
FedericoHurtado <121637596+FedericoHurtado@users.noreply.github.com>
2024-03-18 11:41:12 -0400

    Format file using prettier

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,24 @@
 // entry point for backend
 
 // setup API calls
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-let dotenv = require('dotenv');
+const cors = require("cors");
+let dotenv = require("dotenv");
 
+dotenv.config({});
 
-
-dotenv.config({
-});
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 // routers
-const inputRouter = require('./routes/league');
-const { createDB } = require('./Utils/startup');
+const inputRouter = require("./routes/league");
+const { createDB } = require("./Utils/startup");
 app.use("/league/", inputRouter);
 
 createDB();
 
-// which port to listen on 
+// which port to listen on
 app.listen(3001, () => {
-    console.log("Server running on port 3001"); // function that runs on start up
+  console.log("Server running on port 3001"); // function that runs on start up
 });
