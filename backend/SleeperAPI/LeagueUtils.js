e94df79762215b59de0138d212da73bc5cc4694a
FedericoHurtado <121637596+FedericoHurtado@users.noreply.github.com>
2024-03-25 14:06:01 -0400

    Move validate functionality to LeagueUtils.js

--- backend/SleeperAPI/LeagueUtils.js
+++ backend/SleeperAPI/LeagueUtils.js
@@ -69,4 +69,26 @@ async function getTeamsFromLeague(league_id) {
   return teams;
 }
 
-module.exports = { getTeamsFromLeague };
+async function validateLeague(league_id) {
+  // ensure there is a valid league id
+  if (league_id === null) {
+    return false;
+  }
+
+  try {
+    console.log(league_id);
+
+    // sleeper API returns a null body if a league id does not give a real league
+    const response = await axios.get(
+      `https://api.sleeper.app/v1/league/${league_id}`
+    );
+
+    return response.data !== null;
+  } catch (error) {
+    // in case of error, return false and log it
+    console.log("Error");
+    return false;
+  }
+}
+
+module.exports = { getTeamsFromLeague, validateLeague };
